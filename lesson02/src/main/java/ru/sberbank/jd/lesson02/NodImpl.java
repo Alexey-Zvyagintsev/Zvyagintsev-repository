76eca1c90bd4825fe942406ee23d60e6cba6629b
Vitalii Naumov <hbvit7zoom@gmil.com>
2024-03-10 18:11:54 +0300

    Fixes

--- lesson02/src/main/java/ru/sberbank/jd/lesson02/NodImpl.java
+++ lesson02/src/main/java/ru/sberbank/jd/lesson02/NodImpl.java
@@ -14,24 +14,27 @@ public class NodImpl implements Nod {
      */
     @Override
     public int calculate(int first, int second) {
-        //return 0;
+
         int a = Math.abs(first);
         int b = Math.abs(second);
-        int nod;
+
         if (a == 0 && b == 0) {
-            return -1;
-        } else if (a == 0) {
+            throw new IllegalArgumentException();
+        }
+        if (a == 0) {
             return b;
-        } else if (b == 0) {
+        }
+        if (b == 0) {
             return a;
-        } else {
-            nod = Math.min(a, b);
-            for (int i = nod; i > 0; i--) {
-                if ((a % i == 0) && (b % i == 0)) {
-                    return i;
-                }
+        }
+
+        int nod = Math.min(a, b);
+        for (int i = nod; i > 0; i--) {
+            if ((a % i == 0) && (b % i == 0)) {
+                return i;
             }
         }
+
         return -1;
     }
 }
--- lesson02/src/test/java/ru/sberbank/jd/lesson02/NodImplTest.java
+++ lesson02/src/test/java/ru/sberbank/jd/lesson02/NodImplTest.java
@@ -18,8 +18,12 @@ public class NodImplTest {
         Assert.assertEquals(1, nodTest.calculate(0, 1));
         Assert.assertEquals(1, nodTest.calculate(5, 3));
         Assert.assertEquals(1, nodTest.calculate(1, 0));
-        Assert.assertEquals(-1, nodTest.calculate(0, 0));
         Assert.assertEquals(5, nodTest.calculate(15, 10));
         Assert.assertEquals(2, nodTest.calculate(-8, 6));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void calculateException() {
+        nodTest.calculate(0, 0);
     }
 }
